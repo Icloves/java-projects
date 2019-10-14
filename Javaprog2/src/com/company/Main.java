48c5381f72290699046afa419b58c53e7493d5ba
Hilary Cloves <hcloves@oa-h3.21-school.ru>
2019-10-14 20:33:48 +0300

    1 part

---
+++ Javaprog2/src/com/company/Main.java
@@ -0,0 +1,38 @@
+package com.company;
+import java.util.Scanner;
+import java.util.SplittableRandom;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Text text = new Text();
+        Scanner scanner = new Scanner(System.in);
+        int task;
+        boolean a = true;
+        //String mytext = new String();
+        String mytext = "Князь равнодушно замолк, Анна Павловна. C свойственною ей придворною и женскою ловкостью и быстротою такта, захотела и щелкануть князя за то, что он дерзнул так отозваться о лице, рекомендованном императрице, и в то же время утешить его.\n" +
+                "— Mais à propos de votre famille, — сказала она, — знаете ли, что ваша дочь, с тех пор как выезжает, fait les délices de tout le monde. On la trouve belle comme le jour 16,42.\n" +
+                "Князь наклонился в знак уважения и признательности." +
+                "И она улыбнулась своею восторженной улыбкой.\n" +
+                "— Que voulez-vous? Lafater aurait dit que je n'ai pas la bosse de la paternité 17, — сказал князь.";
+        while (a) {
+            System.out.println("Choose what you want:\n" +
+                    "1. Count the number of paragraphs\n" +
+                    "5. Exit\n");
+            task = scanner.nextByte();
+            switch (task) {
+                case 1: text.paragraph(mytext);
+                    break;
+                case 2: text.timetalk(mytext);
+                    break;
+                case 3: text.parsep(mytext);
+                    break;
+                case 5: a = false;
+                    break;
+            }
+
+        }
+        // write your code here
+    }
+}
---
+++ Javaprog2/src/com/company/Text.java
@@ -0,0 +1,57 @@
+package com.company;
+import java.lang.String;
+import java.util.Arrays;
+
+public class Text {
+    void paragraph(String text){
+        int count = 0;
+        for (int i = 0; i < text.length(); i++)
+            if (text.charAt(i) == '\n')
+                count++;
+        System.out.println("This text have " + count + " paragraphs\n");
+    }
+    void timetalk(String text){
+        int count = 0;
+        int a;
+        for (int i = 0; i < text.length(); i++) {
+            if (text.charAt(i) == '\n')
+                count++;
+        }
+        a = count;
+        int[] myArray = new int[count];
+        count = 0;
+        for (int i = 0; i < text.length(); i++)
+        {
+            if (count >= a)
+                break;
+            if (text.charAt(i) == '\n')
+                count++;
+            if (Character.isDigit(text.charAt(i))) { //use format!!!!!!!!!!!!!!
+                myArray[count] = text.charAt(i) + (myArray[count] * 10) - '0';
+                //count++;
+            }
+            //System.out.print(text.charAt(i) + "");
+        }
+        System.out.println(Arrays.toString(myArray));
+    }
+    void parsep(String text){
+        int count = 0;
+        int a = 0;
+        int i = 0;
+        String newtext[] = new String[text.length()];
+//        for (int i = 0; '\n' != text.charAt(i); i++) {
+//            if (text.charAt(i) == '.' || text.charAt(i) == '!' || text.charAt(i) == '?')
+//                if (text.charAt(i + 1) == ' ' && text.charAt(i + 1) != '\n')
+//                    if (Character.isUpperCase(text.charAt(i + 2)))
+//                        ;
+        while (text.charAt(i) != '\n')
+        {
+            if (text.charAt(i) == '.')
+                newtext[a] = String.valueOf(text.charAt(i));
+            a++;
+            i++;
+        }
+        System.out.println(Arrays.toString(newtext));
+
+    }
+}
