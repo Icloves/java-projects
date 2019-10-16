d6e55609b2619c35c87f6e30da374ad585680506
Hilary Cloves <hcloves@il-e5.21-school.ru>
2019-10-16 23:21:25 +0300

    recode func

--- Javaprog2/src/com/company/Main.java
+++ Javaprog2/src/com/company/Main.java
@@ -10,9 +10,8 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int task;
         boolean a = true;
-        //String mytext = new String();
-        String mytext = "Князь равнодушно замолк. Анна Павловна. C свойственною ей придворною. и женскою ловкостью и быстротою такта. захотела и .щелкануть князя за то., что он дерзнул так отозваться о лице,  . рекомендованном императрице. и в то же время утешить его.\n" +
-                "— Mais à propos de votre famille, — сказала она, — знаете ли, что ваша дочь, с тех пор как выезжает, fait les délices de tout le monde. On la trouve belle comme le jour 16,42.\n" +
+        String mytext = "Князь равнодушно 42.2 замолк. Анна Павловна 423. C свойственною ей придворною 43 . и женскою ловкостью 43 и быстротою такта. захотела и .щелкануть 11 князя за то., что он дерзнул так отозваться о лице,  . рекомендованном 33 императрице. и в то же время 10 утешить его.\n" +
+                "— Mais à propos de votre famille, — сказала она, — знаете ли, что ваша дочь, с тех пор как выезжает, fait les délices de tout le monde. On la trouve belle comme le jour 16.42.\n" +
                 "Князь наклонился в знак уважения и признательности." +
                 "И она улыбнулась своею восторженной улыбкой.\n" +
                 "— Que voulez-vous? Lafater aurait dit que je n'ai pas la bosse de la paternité 17, — сказал князь.";
@@ -21,6 +20,7 @@ public class Main {
                     "1. Count the number of paragraphs\n" +
                     "2. Found the time talks\n" +
                     "3. Split first paragraphs\n" +
+                    "4. Out edit test\n" +
                     "5. Exit\n");
             task = scanner.nextByte();
             switch (task) {
@@ -30,13 +30,11 @@ public class Main {
                     break;
                 case 3: text.parsep(mytext);
                     break;
-                //case 4: text.edittext(mytext);
-                 //   break;
+                case 4: text.edittext();
+                    break;
                 case 5: a = false;
                     break;
             }
-
         }
-        // write your code here
     }
 }
--- Javaprog2/src/com/company/Text.java
+++ Javaprog2/src/com/company/Text.java
@@ -1,72 +1,154 @@
 package com.company;
 import java.lang.String;
-import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Text {
-    void paragraph(String text){
-        int count = 0;
-        for (int i = 0; i < text.length(); i++)
-            if (text.charAt(i) == '\n')
-                count++;
-        System.out.println("This text have " + count + " paragraphs\n");
-    }
-    void timetalk(String text){
-        int count = 0;
-        int a;
-        for (int i = 0; i < text.length(); i++) {
-            if (text.charAt(i) == '\n')
-                count++;
+    String newtext = "";
+    void paragraph(String text) {
+        if (newtext == "") { // operator '==' same with 'equals(oper)'
+            int count = 0;
+            for (int i = 0; i < text.length(); i++)
+                if (text.charAt(i) == '\n')
+                    count++;
+            System.out.println("This text have " + count + " paragraphs\n");
         }
-        a = count;
-        int[] myArray = new int[count];
-        count = 0;
-        for (int i = 0; i < text.length(); i++)
+        else
         {
-            if (count >= a)
-                break;
-            if (text.charAt(i) == '\n')
-                count++;
-            if (Character.isDigit(text.charAt(i))) { //use format!!!!!!!!!!!!!!
-                myArray[count] = text.charAt(i) + (myArray[count] * 10) - '0';
-                //count++;
-            }
-            //System.out.print(text.charAt(i) + "");
+            int count = 0;
+            for (int i = 0; i < newtext.length(); i++)
+                if (newtext.charAt(i) == '\n')
+                    count++;
+            System.out.println("This text have " + count + " paragraphs\n");
         }
-        System.out.println(Arrays.toString(myArray));
     }
-    void parsep(String text){
-        int count = 0;
-        int a = 0;
-        //int i = 0;
-        //String newtext = "";
-        String newtext[] = new String[1];
+    void timetalk(String text) {
         String temp = "";
-        System.out.println(Arrays.toString(newtext));
-        //ArrayList<String> newtext = new ArrayList<String>();
-//        for (int i = 0; '\n' != text.charAt(i); i++) {
-//            //newtext.add(a, String.valueOf(text.charAt(i)));
-//            if (text.charAt(i) == '.' || text.charAt(i) == '!' || text.charAt(i) == '?')
-//                if (text.charAt(i + 1) == ' ' && text.charAt(i + 1) != '\n')
-//                    if (Character.isUpperCase(text.charAt(i + 2))) {
-//                        newtext.add(a, String.valueOf(text.charAt('\n')));
-//                        a++;
-//                    }
+
+        int countpar = 0;
+        for (int i = 0; i < text.length(); i++)
+            if (text.charAt(i) == '\n')
+                countpar++;
+
+        int b = 0;
         int i = 0;
+        while (countpar > 0) {
+            b = i;
+            int count = 0;
+            while (text.charAt(b) != '\n') {
+                if (text.charAt(b) == '.' && (text.charAt(b + 1) == ' ' || text.charAt(b + 1) == '\n'))
+                    count++;
+                b++;
+            }
+            int a = 0;
+            double[] newarr = new double[count];
+            while (text.charAt(i) != '\n') {
+                if (Character.isDigit(text.charAt(i))) {
+                    temp += (text.charAt(i));
+                    if (text.charAt(i + 1) == '.' && (text.charAt(i + 2) != ' ' && text.charAt(i + 2) != '\n')) {
+                        i++;
+                        temp += text.charAt(i);
+                    }
+                }
+                if (text.charAt(i + 1) == '.' && (text.charAt(i + 2) == ' ' || text.charAt(i + 2) == '\n')) {
+                    if (temp == "")
+                        newarr[a] = 0;
+                    else
+                        newarr[a] = Double.parseDouble(temp);
+                    temp = "";
+                    a++;
+                }
+                i++;
+            }
+            int k = 1;
+            while (k < a) {
+                newarr[0] += newarr[k];
+                k++;
+            }
+            countpar--;
+            i++;
+            //System.out.println(count);
+            System.out.println(Arrays.toString(newarr));
+        }
+    }
+
+//        int count = 1;
+//        int a;
+////        if (newtext.equals("")) {
+//            for (int i = 0; i < text.length(); i++) {
+//                if (text.charAt(i) == '\n')
+//                    count++;
+//            }
+//            a = count;
+//            //int[] myArray = new int[count];
+//            String myArray = "";
+//            count = 1;
+//            for (int i = 0; i < text.length(); i++) {
+//                if (count == a)
+//                    break;
+//                if (text.charAt(i) == '\n')
+//                    count++;
+//                if (Character.isDigit(text.charAt(i))) { //use format!!!!!!!!!!!!!!
+//                    //myArray = String.format()
+//                    myArray = String.format("%d", myArray);/*text.charAt(i) + (myArray[count] * 10) - '0';*/ //Если после запятой идет пробел то выходим, иначе дальше чекаем и записываем
+//                    //count++;
+//                }
+//                //System.out.print(text.charAt(i) + "");
+//            }
+//            System.out.println(myArray);
+////        }
+////        else {
+////            count = 1;
+////            for (int i = 0; i < newtext.length(); i++) {
+////                if (newtext.charAt(i) == '\n')
+////                    count++;
+////            }
+////            a = count;
+////            int[] myArray = new int[count];
+////            count = 1;
+////            for (int i = 0; i < newtext.length(); i++) {
+////                if (count == a)
+////                    break;
+////                if (newtext.charAt(i) == '\n')
+////                    count++;
+////                while (Character.isDigit(newtext.charAt(i))) {
+////                    if (newtext.charAt(i + 1) == ',' && newtext.charAt(i + 2) == ' ') {
+////                        myArray[count] = newtext.charAt(i) + (myArray[count] * 10) - '0';
+////                        i++;
+////                        break;
+////                    } else if (newtext.charAt(i + 1) == ',' && Character.isDigit(newtext.charAt(i + 2))) {
+////                        myArray[count] = newtext.charAt(i) + (myArray[count] * 10) - '0';
+////                        i++;
+////                        myArray[count] = newtext.charAt(i);
+////                    } else
+////                        myArray[count] = newtext.charAt(i) + (myArray[count] * 10) - '0';
+////                    i++;
+////                }
+////              1
+//// if (Character.isDigit(newtext.charAt(i))) { //use format!!!!!!!!!!
+////                //  myArray[count] = newtext.charAt(i) + (myArray[count] * 10) - '0'; //Если после запятой идет пробел то выходим, иначе дальше чекаем и записываем
+////                //count++;
+////                //}
+////                //System.out.print(text.charAt(i) + "");
+////            }
+////            System.out.println(Arrays.toString(myArray));
+////        }
+    //}
+
+    void parsep(String text){
+        String temp = "";
+        int i = 0;
+
         while (text.charAt(i) != '\n')
         {
             temp += text.charAt(i);
-            if (text.charAt(i + 1) == '\n'){
-                newtext[0] = temp.replace(". ", ".\n");
-            }
+            if (text.charAt(i + 1) == '\n')
+                newtext = temp.replace(". ", ".\n");
             i++;
         }
-        newtext[0] += '\n';
-        newtext[0] += text.substring('\n');
-        System.out.println(Arrays.toString(newtext));
+        newtext += text.substring(i);
     }
-//    void edittext(String text, String newtext)
-//    {
-//
-//    }
+    void edittext()
+    {
+        System.out.println(newtext);
+    }
 }
